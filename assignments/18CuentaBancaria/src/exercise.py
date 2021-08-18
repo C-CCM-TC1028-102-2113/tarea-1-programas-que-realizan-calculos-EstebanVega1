c6925d549265c83bbb97d0ae27a086226e151872
EstebanVega1 <89094691+EstebanVega1@users.noreply.github.com>
2021-08-17 19:21:10 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,6 +1,15 @@
-def main():
-    #escribe tu código abajo de esta línea
-    pass
+import math
+saldo_anterior=int(input("Cuanto fue su saldo anterior:"))
+ingresos=int(input("Cuanto son sus ingresos:"))
+egresos=int(input("Cuanto son sus egresos:"))
+cheques=int(input("Cantidad de cheques:"))
 
-if __name__ == '__main__':
-    main()
+cheque1=(cheques+13)
+costo=(saldo_anterior+ingresos+egresos+cheque1)
+porcentaje=((costo*7.5)/100)
+
+costo2=(porcentaje+costo)
+
+print("El saldo final de su cuenta es de:")
+print(costo2)
+
