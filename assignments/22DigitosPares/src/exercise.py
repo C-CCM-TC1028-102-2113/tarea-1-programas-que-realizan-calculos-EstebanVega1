0bdd4c80cb68408d123567edcbe228ee050b7c90
EstebanVega1 <89094691+EstebanVega1@users.noreply.github.com>
2021-08-22 17:00:33 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,7 +1,19 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+entrada = int(input("Ponga un numero de 4 digitos:"))
 
+par = 0
+impar = 0
+
+while(entrada > 0):
+  if entrada % 2 == 0:
+    par += 1
+  else:
+    impar += 1
+  entrada = entrada // 10
+
+print(f"Numero de numeros pares: {par} | Numero de numeros impares: {impar}")
 
 if __name__ == '__main__':
     main()
